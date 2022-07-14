d8ef9aaf09f71c6d7199946f709bde65506cdd22
CG <cgabriel.lien@gmail.com>
2022-07-14 17:52:26 -0400

    add interchange

--- src/components/MatrixRow.js
+++ src/components/MatrixRow.js
@@ -9,19 +9,20 @@ const MatrixRow = (props) => {
     props.selected ? styles.selected : ""
   }`;
 
-  //   const ref = createRef();
+  const ref = createRef();
 
   const onClickHandler = (e) => {
     // setSelection(!selected);
     // console.log(e.clientX, e.clientY);
     // console.log(e.target.getBoundingClientRect());
+    // console.log();
     // let y0 = 297;
     // let y1 = 368;
     // let dy = y1 - y0;
     // e.target.style = `top: ${dy}px; transition: top 0.25s ease-in-out;`;
     // e.target.style = `top: ${dy}px; transition: top 0.25s ease-out;`;
     // props.callback(e.target, props.index);
-    props.callback(e, props.id, props.index);
+    props.callback(e.target, props.id, props.index);
     // props.callback(props.id, props.index);
   };
 
@@ -33,7 +34,7 @@ const MatrixRow = (props) => {
       onClick={(e) => {
         onClickHandler(e);
       }}
-      //   ref={ref}
+      ref={ref}
       className={classes}
     >
       {values}
--- src/components/Workspace.js
+++ src/components/Workspace.js
@@ -10,17 +10,48 @@ import { useState } from "react";
 // import { Fragment } from "react";
 
 const Workspace = () => {
-  const matrix = [
+  //   const matrix = [
+  //     [1, 2, 3, 4],
+  //     [2, 3, 4, 5],
+  //     [3, 4, 5, 6],
+  //   ];
+
+  const [matrix, setMatrix] = useState([
     [1, 2, 3, 4],
     [2, 3, 4, 5],
     [3, 4, 5, 6],
-  ];
-
+  ]);
   const toolbox = [
     {
       name: "Interchange",
       callback: () => {
         console.log("interchange");
+        console.log(selection);
+        const A = selection[0].element;
+        const B = selection[1].element;
+        let y0 = A.getBoundingClientRect().y;
+        let y1 = B.getBoundingClientRect().y;
+        // console.log(y0, y1, y1 - y0, y0 - y1);
+        const dy = y1 - y0;
+        A.classList.toggle(`${rowStyles.moving}`);
+        B.classList.toggle(`${rowStyles.moving}`);
+        A.style = `top: ${dy}px`;
+        B.style = `top: -${dy}px`;
+        setTimeout(() => {
+          setSelection([null, null]);
+          setRowSelections([...rowSelection].fill(false));
+          const newMatrix = [...matrix];
+          const temp = newMatrix[0];
+          newMatrix[0] = newMatrix[1];
+          newMatrix[1] = temp;
+          setMatrix(newMatrix);
+          A.classList.toggle(`${rowStyles.moving}`);
+          B.classList.toggle(`${rowStyles.moving}`);
+          A.style = "";
+          B.style = "";
+        }, 1000);
+        // A.style = "";
+        // B.style = "";
       },
     },
     {
